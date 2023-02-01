d0f83678325f42139ee4d4dde62927c610208a26
Hybusa <alkouzne007@gmail.com>
2023-02-01 21:14:18 +0300

    base difficulty done.

---
+++ src/Employee.java
@@ -0,0 +1,67 @@
+public class Employee {
+
+    private static int ID;
+
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final int employeeID;
+
+    int department;
+    int salary;
+
+
+    public Employee(String firstName, String middleName, String lastName, int department, int salary) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        this.employeeID = ID;
+        ID++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s %s %s%n ID: %d%n Department: %d%n Salary: %d%n%n",
+                this.lastName,
+                this.firstName,
+                this.middleName,
+                this.employeeID,
+                this.department,
+                this.salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Nicolle", "Galilea", "Hoskins",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[1] = new Employee("Tate", "Antony", "Villalobos",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[2] = new Employee("Valencia", "Claire", "Alonso",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[3] = new Employee("Noa", "Marlene", "Pina",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[4] = new Employee("Zariah", "Eunice", "Boswell",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[5] = new Employee("Paul", "Nevin", "Bragg",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[6] = new Employee("Quinn", "Brandi", "Clemons",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[7] = new Employee("Kyler", "Nasir", "Satterfield",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[8] = new Employee("Keara", "Selene", "Champion",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+        employees[9] = new Employee("Elisha", "Ulysses", "Salazar",
+                getRandomIntInRange(1,5),getRandomIntInRange(100_000,500_000));
+
+        printAllEmployees(employees);
+        System.out.println("total: "+ payrollExpense(employees));
+        System.out.println();
+        System.out.println("min: "+ minSalary(employees));
+        System.out.println("max: "+ maxSalary(employees));
+        System.out.println("avarageSalary: "+ avarageSalary(employees));
+
+    }
+
+    public static void printAllEmployees(Employee[] employees) {
+        for (Employee employee : employees)
+            System.out.println(employee);
+    }
+
+    public static int payrollExpense(Employee[] employees) {
+        int totalPayrollExpense = 0;
+        for (Employee employee : employees) {
+            totalPayrollExpense += employee.salary;
+        }
+        return totalPayrollExpense;
+    }
+
+    public static Employee minSalary(Employee[] employees) {
+        Employee[] sortedEmployees = Arrays.copyOf(employees, employees.length);
+        return sortedEmployees[0];
+
+    }
+
+    public static Employee maxSalary(Employee[] employees) {
+        Employee[] sortedEmployees = Arrays.copyOf(employees, employees.length);
+        return sortedEmployees[employees.length - 1];
+    }
+
+    public static int avarageSalary(Employee[] employees) {
+        return payrollExpense(employees) / employees.length;
+    }
+
+    public static void fillEmployeeFieldsWithRandoms(Employee[] employees)
+    {
+        for (Employee employee : employees) {
+            employee.salary = getRandomIntInRange(250_000, 500_000);
+            employee.department = getRandomIntInRange(1, 5);
+        }
+    }
+
+    public static int getRandomIntInRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
     }
 }
